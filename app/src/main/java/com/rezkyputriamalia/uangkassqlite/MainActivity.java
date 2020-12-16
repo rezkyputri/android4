42a1024b1c0554356c8c7036773056d120915577
rezkyputri <rezkyputri611@gmail.com>
2020-12-17 00:15:39 +0700

    Menambahkan Splash Screen, Edit, Add, Filter Activity

--- app/src/main/java/com/rezkyputriamalia/uangkassqlite/MainActivity.java
+++ app/src/main/java/com/rezkyputriamalia/uangkassqlite/MainActivity.java
@@ -1,5 +1,6 @@
 package com.rezkyputriamalia.uangkassqlite;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -26,8 +27,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+              startActivity(new Intent(MainActivity.this, AddActivity.class));
             }
         });
     }
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_filter) {
             return true;
         }
 
---
+++ app/src/main/java/com/rezkyputriamalia/uangkassqlite/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.rezkyputriamalia.uangkassqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread waktu = new Thread(){
+
+            @Override
+            public void run() {
+               try {
+                   sleep(3000);
+               }catch (InterruptedException e){
+                   e.printStackTrace();
+               }finally {
+                   startActivity(new Intent(SplashActivity.this, MainActivity.class));
+               }
+            }
+        };
+        waktu.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
